cd1b854dad9adf11a4d22bdf5c1e43fbe4253459
coral00685 <coral00685@gmail.com>
2017-06-20 22:16:31 +0800

    Add files via upload

---
+++ Lab_LoginSession/Login.aspx.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class Login : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+    protected void btnLogin_Click(object sender, EventArgs e)
+    {
+        if (txtUserName.Text.Trim() == "")
+        {
+            lblPrompt.Text = "Please keyin your name.";
+            return;
+        }
+
+        Session["userName"] = txtUserName.Text;
+
+        if (Session["returnUrl"] == null)
+        {
+            Response.Redirect("Default.aspx");
+        }
+        else {
+            Response.Redirect(Session["returnUrl"].ToString());
+        }
+
+    }
+}
